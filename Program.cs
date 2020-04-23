bd7e0eb3350ec3326502fa2cacf3e8d6df314a9b
Jan3r <janerleonardo@gmail.com>
2020-04-23 07:19:45 -0500

    Utilizacion de Predicado, Delegado, Lambda en una busqueda en Listas (List)

--- Program.cs
+++ Program.cs
@@ -10,36 +10,44 @@ namespace CoreEscuela
         static void Main(string[] args)
         {
             var escuela = new Escuela("Platzi","Ave siempre viva",2020,"Janer");
+            escuela.Cursos = new List<Curso>(){
+                        new Curso(){ Nombre = "101", Jornada = Jornada.Mañana },
+                        new Curso() {Nombre = "201", Jornada = Jornada.Mañana},
+                        new Curso() {Nombre = "301", Jornada = Jornada.Tarde},
+                        new Curso() {Nombre = "311", Jornada = Jornada.Tarde},
+                        new Curso() {Nombre = "312", Jornada = Jornada.Tarde},
+                        new Curso() {Nombre = "313", Jornada = Jornada.Tarde},
+                        new Curso() {Nombre = "401", Jornada = Jornada.Noche},
+                        new Curso() {Nombre = "501", Jornada = Jornada.Mañana},
+                        new Curso() {Nombre = "601", Jornada = Jornada.Tarde},
+                        new Curso {  Nombre = "701", Jornada = Jornada.Noche}
+            };
+
+            //Forma de elimina un registro de la lista utilizando un  predicado 
+            escuela.Cursos.RemoveAll(Predicate);
+            // Forma de eliminar utilizando un Delegado
+            escuela.Cursos.RemoveAll(delegate(Curso curso) {
+                return curso.Nombre == "312";
+            });
+            //Forma de eliminar utilizando expresiones lambda
+            escuela.Cursos.RemoveAll((Curso cur) => cur.Nombre == "313" );
+            ImpreCurso(escuela.Cursos);
            
             
 
         }
 
-        public void Cursitos(string parameter)
+        private static bool Predicate(Curso obj)
         {
-            var curso = new Curso(){
-                Nombre = "101",
-                Jornada = Jornada.Mañana
-            };
-            var curso1 = new Curso(){
-                Nombre = "201",
-                Jornada = Jornada.Tarde
-            };
-            var curso2 = new Curso(){
-                Nombre = "301",
-                Jornada = Jornada.Tarde
-            };
-            var curso3 = new Curso(){
-                Nombre = "401",
-                Jornada = Jornada.Tarde
-            };
-            var curso4 = new Curso(){
-                Nombre = "401"
-            };
-            List<Curso> listCurso = new List<Curso>(){curso,curso1,curso2,curso3,curso4};
+            return obj.Nombre == "311";
+        }
+
+        public static void ImpreCurso(List<Curso> listCurso)
+        {
 
             foreach (var item in listCurso)
             {
+                //Formas de Impresion
                  System.Console.WriteLine(string.Format("Curso:  {0}, Id: {1}, \n Jornada {2}", item.Nombre,item.Id,item.Jornada));
                  System.Console.WriteLine($"Curso {item.Nombre}, \n Id {item.Id}, {System.Environment.NewLine} Jornada {item.Jornada}");
             }
